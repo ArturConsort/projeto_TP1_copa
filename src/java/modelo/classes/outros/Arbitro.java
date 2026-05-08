c34cb1a914618952f854cf07161391ad282384a1
Mariana <190521953+mariana-farias12@users.noreply.github.com>
2026-05-08 20:52:43 -0300

    feat: adiciona classes arbitro e estadio

---
+++ src/java/modelo/classes/outros/Arbitro.java
@@ -0,0 +1,32 @@
+package modelo.classes.outros;
+
+enum Categoria{
+    FIFA,
+    NACIONAL
+}
+
+public class Arbitro{
+    private String nome;
+    private int idade;
+    private Categoria categoria;
+    private int experiencia;
+
+    public Arbitro(String nome, int idade, Categoria categoria, int experiencia){
+        this.nome = nome;
+        this.idade = idade;
+        this.categoria = categoria;
+        this.experiencia = experiencia;
+    }
+
+    public String getNome() {return nome;}
+    public void setNome(String nome) {this.nome = nome;}
+
+    public int getIdade() {return idade;}
+    public void setIdade(int idade) {this.idade = idade;}
+
+    public Categoria getCategoria() {return categoria;}
+    public void setCategoria(Categoria categoria) {this.categoria = categoria;}
+
+    public int getExperiencia() {return experiencia;}
+    public void setExperiencia(int experiencia) {this.experiencia = experiencia;}
+}
---
+++ src/java/modelo/classes/outros/Estadio.java
@@ -0,0 +1,48 @@
+package modelo.classes.outros;
+
+enum Tipo_gramado{
+    SINTÉTICO,
+    NATURAL
+}
+public class Estadio{
+    private String nome, cidade, estado;
+    private int capacidade;
+    private Tipo_gramado tipo_gramado;
+    // private String image_path; --> será discutido
+
+    // constructor
+    public Estadio(String nome, String cidade, String estado, int capacidade, Tipo_gramado tipo_gramado){
+        this.nome = nome;
+        this.cidade = cidade;
+        this.estado = estado;
+        this.capacidade = capacidade;
+        this.tipo_gramado = tipo_gramado;
+    }
+
+    // getters e setters
+    public String getNome() { return nome; }
+    public void setNome(String nome) { this.nome = nome; }
+
+    public String getCidade() { return cidade; }
+    public void setCidade(String cidade) { this.cidade = cidade; }
+
+    public String getEstado() { return estado; }
+    public void setEstado(String estado) { this.estado = estado; }
+
+    public int getCapacidade() { return capacidade; }
+    public void setCapacidade(int capacidade) { this.capacidade = capacidade; }
+
+    public Tipo_gramado getTipo_gramado() { return tipo_gramado; }
+    public void setTipo_gramado(Tipo_gramado tipo_gramado) { this.tipo_gramado = tipo_gramado; }
+
+    // functions
+    public String getLocalizacao(){
+        return String.format("Localização: %s, %s", this.cidade, this.estado);
+
+    }
+    public boolean aceitaPublico(int quantidade) {
+        return quantidade <= this.capacidade;
+    }
+
+
+}
