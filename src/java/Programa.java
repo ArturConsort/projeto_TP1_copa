b64fb57c8c7ae504d0aed8c4acbfa20f233d457a
Artur Consort <arturconsort@gmail.com>
2026-05-30 11:58:50 -0300

    funcionalidades da main

--- src/java/Programa.java
+++ src/java/Programa.java
@@ -2,6 +2,7 @@ package src.java;
 
 import src.java.modelo.classes.Usuario;
 import src.java.modelo.enumerations.TipoPerfil;
+import src.java.persistencia.UsuarioDAO;
 import src.java.servicos.usuario.Relatorio;
 import src.java.servicos.usuario.SessaoUsuario;
 import src.java.servicos.usuario.UsuarioServico;
@@ -11,10 +12,11 @@ import java.util.Scanner;
 
 public class Programa {
 
+    private static Scanner entrada = new Scanner(System.in);
+    private static UsuarioServico servico = new UsuarioServico();
+
     public static void main(String[] args){
 
-        Scanner entrada = new Scanner(System.in);
-        UsuarioServico servico = new UsuarioServico();
         limparTela();
 
 
@@ -40,10 +42,13 @@ public class Programa {
         Usuario logado = SessaoUsuario.getInstancia().getUsuarioLogado();
         TipoPerfil perfil = logado.getPerfil();
 
+        boolean opInvalida = false;
         boolean rodando = true;
         while (rodando){
 
             limparTela();
+            if(opInvalida == true) System.out.println("= opção invalida =");
+            opInvalida = false;
             System.out.println("=== Bem vindo " + logado.getNome() + " ===");
             exibeMenu(logado.getPerfil());
 
@@ -52,15 +57,73 @@ public class Programa {
 
                 case "1" -> listarUsuarios();
                 case "2" -> gerarRelatorio();
-                case "3" -> gerirPartida();
-                case "4" -> gerirSelecao();
-                case "5" -> visualizarPartida();
-                case "6" -> comprarIngressos();
-                case "7" -> verRegistros();
-                case "8" -> criarConta();
-                case "9" -> removerConta();
 
-                case "0" -> servico.logout();
+                case "3" -> {
+                    if (perfil == TipoPerfil.ADMINISTRADOR || perfil == TipoPerfil.ORGANIZADOR)
+                        gerirPartida();
+                    else opInvalida = true;
+                }
+
+
+                case "4" -> {
+                    if (perfil == TipoPerfil.ADMINISTRADOR || perfil == TipoPerfil.ORGANIZADOR)
+                        gerirSelecao();
+                    else opInvalida = true;
+                }
+
+
+                case "5" -> {
+                    if (perfil == TipoPerfil.ADMINISTRADOR || perfil == TipoPerfil.ARBITRO)
+                        visualizarPartida();
+                    else opInvalida = true;
+                }
+
+
+                case "6" -> {
+                    if (perfil == TipoPerfil.ADMINISTRADOR || perfil == TipoPerfil.OPERADOR)
+                        comprarIngressos();
+                    else opInvalida = true;
+                }
+
+
+                case "7" -> {
+                    if (perfil == TipoPerfil.ADMINISTRADOR || perfil == TipoPerfil.OPERADOR)
+                        verRegistros();
+                    else opInvalida = true;
+                }
+
+
+                case "8" -> {
+                    if (perfil == TipoPerfil.ADMINISTRADOR)
+                        criarConta();
+                    else opInvalida = true;
+                }
+
+
+                case "9" -> {
+                    if (perfil == TipoPerfil.ADMINISTRADOR)
+                        removerConta();
+                    else opInvalida = true;
+                }
+
+
+                case "10" -> {
+                    if (perfil == TipoPerfil.ADMINISTRADOR)
+                        editaConta();
+                    else opInvalida = true;
+                }
+
+
+                case "0" ->{
+                    entrada.close();
+                    servico.logout();
+                    rodando = false;
+                    System.out.println("Sessão encerrada");
+                }
+
+                default -> opInvalida = true;
+
+
 
             }
 
@@ -119,9 +182,6 @@ public class Programa {
 
 
     private static void listarUsuarios(){
-
-        Scanner entrada = new Scanner(System.in);
-        UsuarioServico servico = new UsuarioServico();
 
         try {
             while(true) {
@@ -187,7 +247,6 @@ public class Programa {
 
     private static void gerarRelatorio(){
 
-        Scanner entrada = new Scanner(System.in);
         Relatorio relatorio = new Relatorio();
         limparTela();
 
@@ -224,7 +283,6 @@ public class Programa {
 
 
     private static void gerirPartida(){
-        Scanner entrada = new Scanner(System.in);
         limparTela();
         System.out.println("SESSAO EM DESENVOLVIMENTO");
         System.out.println();
@@ -233,7 +291,6 @@ public class Programa {
     }
 
     private static void gerirSelecao(){
-        Scanner entrada = new Scanner(System.in);
         limparTela();
         System.out.println("SESSAO EM DESENVOLVIMENTO");
         System.out.println();
@@ -242,7 +299,6 @@ public class Programa {
     }
 
     private static void visualizarPartida(){
-        Scanner entrada = new Scanner(System.in);
         limparTela();
         System.out.println("SESSAO EM DESENVOLVIMENTO");
         System.out.println();
@@ -251,7 +307,6 @@ public class Programa {
     }
 
     private static void comprarIngressos(){
-        Scanner entrada = new Scanner(System.in);
         limparTela();
         System.out.println("SESSAO EM DESENVOLVIMENTO");
         System.out.println();
@@ -260,7 +315,6 @@ public class Programa {
     }
 
     private static void verRegistros(){
-        Scanner entrada = new Scanner(System.in);
         limparTela();
         System.out.println("SESSAO EM DESENVOLVIMENTO");
         System.out.println();
@@ -270,8 +324,6 @@ public class Programa {
 
     private static void criarConta(){
 
-        Scanner entrada = new Scanner(System.in);
-        UsuarioServico servico = new UsuarioServico();
         limparTela();
 
         while(true) {
@@ -341,36 +393,52 @@ public class Programa {
 
     private static void removerConta(){
 
-        Scanner entrada = new Scanner(System.in);
-        UsuarioServico servico = new UsuarioServico();
         limparTela();
 
         while(true) {
 
 
-
             System.out.print("Login do usuário a remover: ");
             String login = entrada.nextLine();
+            limparTela();
+
+            Usuario encontrado = servico.buscarPorLogin(login);
+
+            if(encontrado == null){
+                System.out.println("Nenhum usuário encontrado com o login \"" + login + "\".");
+                System.out.println("(1) Tentar outro login");
+                System.out.println("(2) Voltar ao menu");
+                System.out.print("Escolha uma opção: ");
+                String op = entrada.nextLine();
+                limparTela();
+                if (op.equals("2")) break;
+                continue;
+            }
+
+
 
             System.out.println();
 
             try {
 
+                System.out.println("Usuário encontrado: " + encontrado.getNome() + " | " + encontrado.getPerfil());
                 System.out.println("ANTENÇÃO, VOCÊ ESTÁ PRESTES A REMOVER UMA CONTA PERMANENTEMENTE,");
                 System.out.println("(1) desistir de remover a conta");
                 System.out.println("(2) remover a conta permanentemente");
                 String op = entrada.nextLine();
+                limparTela();
 
                 if (op.equals("2")){
                     servico.excluir(login);
-                    System.out.println("conta removida");
+                    System.out.println("Conta removida");
                 }
+                else System.out.println("Remoção cancelada");
             }
             catch (Exception e) {
                 System.out.println("Erro: " + e.getMessage());
             }
 
-            System.out.println("(1) Remover outro usuário");
+            System.out.println("(1) Remover usuário");
             System.out.println("(2) Voltar ao menu");
             System.out.print("Escolha uma opção: ");
             String op = entrada.nextLine();
@@ -387,7 +455,100 @@ public class Programa {
 
 
 
+    public static void editaConta(){
 
+        limparTela();
+
+        while(true){
+
+            System.out.print("Login do usuário a editar: ");
+            String login = entrada.nextLine();
+            limparTela();
+
+            Usuario encontrado = servico.buscarPorLogin(login);
+
+            if(encontrado == null){
+                System.out.println("Nenhum usuário encontrado com o login \"" + login + "\".");
+                System.out.println("(1) Tentar outro login");
+                System.out.println("(2) Voltar ao menu");
+                System.out.print("Escolha uma opção: ");
+                String op = entrada.nextLine();
+                limparTela();
+                if (op.equals("2")) break;
+                continue;
+            }
+
+            System.out.println("=== DADOS ATUAIS ===");
+            System.out.println("Nome:   " + encontrado.getNome());
+            System.out.println("CPF:    " + encontrado.getCpf());
+            System.out.println("Email:  " + encontrado.getEmail());
+            System.out.println("País:   " + encontrado.getPais());
+            System.out.println("senha:   " + encontrado.getSenha());
+            System.out.println("Perfil: " + encontrado.getPerfil());
+            System.out.println();
+            System.out.println("Digite o novo valor para cada campo.");
+            System.out.println("Deixe em branco para manter o valor atual.");
+            System.out.println();
+
+            System.out.print("Nome:   ");
+            String nome = entrada.nextLine();
+
+            System.out.print("CPF:    ");
+            String cpf = entrada.nextLine();
+
+            System.out.print("Email:  ");
+            String email = entrada.nextLine();
+
+            System.out.print("País:   ");
+            String pais = entrada.nextLine();
+
+            System.out.print("Senha:   ");
+            String senha = entrada.nextLine();
+
+            System.out.print("Perfil (ADMINISTRADOR, ORGANIZADOR, OPERADOR, ARBITRO): ");
+            String temp = entrada.nextLine().toUpperCase();
+
+            TipoPerfil perfil = null;
+            if (!temp.isBlank()) {
+                try{
+                    perfil = TipoPerfil.valueOf(temp.toUpperCase());
+                }
+                catch(IllegalArgumentException e){
+                    System.out.println("Perfil inválido, ignorando filtro.");
+                }
+            }
+
+            if(nome.isBlank()) nome = null;
+            if(cpf.isBlank()) cpf = null;
+            if(email.isBlank()) email = null;
+            if(pais.isBlank())pais = null;
+            if(senha.isBlank())senha = null;
+
+            try{
+                List<String> avisos = servico.editar(login, nome, cpf, email, pais, senha, perfil);
+                limparTela();
+                System.out.println("= Edição concluída =");
+                if(!avisos.isEmpty()) avisos.forEach(a -> System.out.println("aviso: " + a));
+            }
+            catch(Exception e){
+                System.out.println("Erro: " + e.getMessage());
+            }
+
+            System.out.println();
+            System.out.println("(1) Editar outro usuário");
+            System.out.println("(2) Voltar ao menu");
+            System.out.print("Escolha uma opção: ");
+            String op = entrada.nextLine();
+            limparTela();
+
+            if (op.equals("2")) break;
+
+
+
+
+        }
+
+    }
 
 
 
