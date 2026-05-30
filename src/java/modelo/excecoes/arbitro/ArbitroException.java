18dd5586527ee27cc55c7e021edb0176790d001c
Mariana <190521953+mariana-farias12@users.noreply.github.com>
2026-05-30 18:25:49 -0300

    feat: adiciona exceções de validação das entidades

---
+++ src/java/modelo/excecoes/arbitro/ArbitroException.java
@@ -0,0 +1,7 @@
+package src.java.modelo.excecoes.arbitro;
+
+public class ArbitroException extends Exception {
+    public ArbitroException(String mensagem) {
+        super(mensagem);
+    }
+}
---
+++ src/java/modelo/excecoes/estadio/EstadioException.java
@@ -0,0 +1,7 @@
+package src.java.modelo.excecoes.estadio;
+
+public class EstadioException extends Exception{
+    public EstadioException(String mensagem){
+        super(mensagem);
+    }
+}
